3536b39b26b1b1f7e6ac4e6ce887b9d2a53b23c3
Ricardo Oneda <20815222+roneda@users.noreply.github.com>
2023-08-06 21:07:55 +0000

    Adicionado devcontainer e alterações nos testes

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,3 +1,25 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 // updated imports added for convenience
+
+const assert = require('assert');
+const web3 = new Web3(ganache.provider());
+
+const {interface, bytecode} =  require ('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach (async ()=>{
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode})
+        .send({ from:accounts[0], gas: '1000000'});
+})
+
+describe ('Lottery contract', ()=> {
+    it('It deploys a contract', ()=>{
+        assert.ok(lottery.options.address);
+    })
+})
